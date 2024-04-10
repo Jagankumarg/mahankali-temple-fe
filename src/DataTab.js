9e2fab50a4aa572b878092f1c53c8b999708b647
Jagankumarg <jagan0523@gmail.com>
2024-04-10 23:56:06 +0530

    added border collapse

--- src/DataTab.js
+++ src/DataTab.js
@@ -45,7 +45,7 @@ const DataTab = ({language }) => {
       {error && <p>Error: {error}</p>}
       {data.length > 0 && (
         <div>
-          <table style={{ minWidth: '250px', tableLayout: 'fixed', width: '300%'}}>
+          <table style={{ minWidth: '250px', tableLayout: 'fixed', width: '250%'}}>
           
             <thead>
               <tr>
