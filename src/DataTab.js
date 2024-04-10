6c76ca4c560e6dca8bc5f820035eaa362e9c716d
Jagankumarg <jagan0523@gmail.com>
2024-04-10 19:34:01 +0530

    udpated horizontal scrolling2

--- src/DataTab.js
+++ src/DataTab.js
@@ -44,7 +44,7 @@ const DataTab = ({language }) => {
       {loading && <p>Loading data...</p>}
       {error && <p>Error: {error}</p>}
       {data.length > 0 && (
-        <div>
+        <div style={{ overflowX: 'auto', width: '100%' }}>
           <table style={{ minWidth: '100px', tableLayout: 'fixed', whiteSpace: 'nowrap'}}>
             <thead>
               <tr>
